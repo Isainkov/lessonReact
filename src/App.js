968f5a4093dd88d51589fddeec3970e1e0d90406
MariaChernousova <chernousova.maria.kr@gmail.com>
2021-02-25 11:49:33 +0200

    rewriting to react approach

--- src/App.js
+++ src/App.js
@@ -2,46 +2,214 @@ import React, { Component } from "react";
 import "./styles/index.scss";
 import Navigation from "./Navigation/Navigation";
 import Table from "./Table/Table";
-import Modal from "./VacationModal/Modal"
+import Modal from "./VacationModal/Modal";
+// import { calendarFetchHandler } from "./API/api";
+import { Utils } from "./Utils/utils";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
         currentDate: new Date(),
-        modalActive: true
+        modalActive: false,
+        width: 31,
+        teams: null
     };
 
     this.nextMonth = this.nextMonth.bind(this);
     this.prevMonth = this.prevMonth.bind(this);
     this.setModalActive = this.setModalActive.bind(this);
+    this.calendarFetchHandler = this.calendarFetchHandler.bind(this);
+}
+
+componentDidMount() {
+  this.calendarFetchHandler();
+}
+
+calendarFetchHandler = () => {
+  const departmentTeams = {
+    teams: [
+      {
+        name: "Frontend Team",
+        percentageOfAbsent: [0, 2, 0, 0, 1, 22, 2, 2, 2, 2, 11, 1],
+        members: [
+          {
+            name: "FE_Team_User1",
+            vacations: [
+              {
+                startDate: "20.12.2020",
+                endDate: "22.12.2020",
+                type: "Paid",
+              },
+              {
+                startDate: "20.11.2020",
+                endDate: "22.11.2020",
+                type: "Paid",
+              },
+            ],
+          },
+          {
+            name: "FE_Team_User1",
+            vacations: [
+              {
+                startDate: "20.02.2020",
+                endDate: "22.02.2020",
+                type: "UnPaid",
+              },
+              {
+                startDate: "10.03.2020",
+                endDate: "16.03.2020",
+                type: "UnPaid",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Backend Team",
+        percentageOfAbsent: [0, 2, 0, 0, 1, 2, 2, 2, 2, 2, 1, 1],
+        members: [
+          {
+            name: "FE_Team_User1",
+            vacations: [
+              {
+                startDate: "15.02.2020",
+                endDate: "22.02.2020",
+                type: "UnPaid",
+              },
+              {
+                startDate: "20.03.2020",
+                endDate: "22.03.2020",
+                type: "UnPaid",
+              },
+            ],
+          },
+          {
+            name: "FE_Team_User1",
+            vacations: [
+              {
+                startDate: "20.02.2020",
+                endDate: "22.02.2020",
+                type: "UnPaid",
+              },
+              {
+                startDate: "02.04.2020",
+                endDate: "12.04.2020",
+                type: "UnPaid",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Designers",
+        percentageOfAbsent: [0, 2, 0, 0, 1, 2, 2, 2, 2, 2, 1, 1],
+        members: [
+          {
+            name: "FE_Team_User1",
+            vacations: [
+              {
+                startDate: "10.02.2020",
+                endDate: "15.02.2020",
+                type: "UnPaid",
+              },
+              {
+                startDate: "20.03.2020",
+                endDate: "22.03.2020",
+                type: "UnPaid",
+              },
+            ],
+          },
+          {
+            name: "FE_Team_User1",
+            vacations: [
+              {
+                startDate: "01.02.2020",
+                endDate: "06.02.2020",
+                type: "UnPaid",
+              },
+              {
+                startDate: "20.03.2020",
+                endDate: "22.03.2020",
+                type: "UnPaid",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+  return fetch("https://jsonplaceholder.typicode.com/posts/1", {
+      method: "PUT",
+      body: JSON.stringify(departmentTeams),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({teams: json.teams});
+      }).catch(err => console.log(err));
 }
 
 
-prevMonth() {
-    this.setState(() => {
-        return {
+prevMonth = () => {
+    this.setState({
             currentDate: new Date(this.state.currentDate.getFullYear(), this.state.currentDate.getMonth() - 1, 1)
-        }
     });
 }
 
-nextMonth() {
-    this.setState(() => {
-        return {
+nextMonth = () => {
+    this.setState({
             currentDate: new Date(this.state.currentDate.getFullYear(), this.state.currentDate.getMonth() + 1, 1)
-        }
     });
 }
 
-setModalActive(value){
+setModalActive = (value) => {
   this.setState({modalActive: value})
-}  
+}
+
+getDaysOfActivePeriod() {
+  const year = this.state.currentDate.getFullYear();
+  const month = this.state.currentDate.getMonth();
+  const date = new Date(year, month, 1);
+  const days = [];
+  while (date.getMonth() === month) {
+    const item = new Date(date);
+    days.push({
+      dayName: item.toLocaleDateString("en-US", {weekday: "short"}),
+      date: item.getDate(),
+      isDayOff: Utils.isWeekend(item),
+      fullDate: item,
+    });
+    date.setDate(date.getDate() + 1);
+  }
+  return days
+}
+
+// getCurrentPeriod = () => {
+//   const month = Number.parseInt(this.state.currentDate.toLocaleDateString("en-US", { month: "numeric" }), 10);
+//   const year = Number.parseInt(this.state.currentDate.toLocaleDateString("en-US", { year: "numeric" }), 10);
+//   const countDays = Utils.getDaysInMonth(month, year);
+//   const cells = [];
+//   const date = new Date(year, month, 1);
+//   while (cells.length !== countDays){
+//     const item = new Date(date);
+//     cells.push({
+//       weekdayName: item.toLocaleDateString("en-US", { weekday: "short" }),
+//       // hiddenDay: Utils.hiddenDays(item),
+//       weekendDay: Utils.isWeekend(item)
+//     });
+//   }
+//   return cells;
+
+// }
+
   render() {
     return (
       <div className="App">
         <Navigation currentDate={this.state.currentDate} prevMonth = {this.prevMonth} nextMonth = {this.nextMonth}></Navigation>
-        <Table currentDate={this.state.currentDate} setModalActive={this.setModalActive}></Table>
+        <Table allDays={this.getDaysOfActivePeriod()} currentDate={this.state.currentDate} teams={this.state.teams} setModalActive={this.setModalActive} width={this.state.width}></Table>
          <Modal active={this.state.modalActive} setModalActive={this.setModalActive} />
       </div>
     );
--- src/Table/Table.js
+++ src/Table/Table.js
@@ -4,35 +4,49 @@ import TableBody from './TableBody';
 import { Utils} from '../Utils/utils';
 
 
-class Table extends Component {
-  constructor(props) {
-    super(props);
+// class Table extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      width: 33,
-      // month: '',
-      // year: '',
-      // countDays: ''
-    };
-    this.month = Number.parseInt(this.props.currentDate.toLocaleDateString("en-US", { month: "numeric" }), 10);
-    this.year = Number.parseInt(this.props.currentDate.toLocaleDateString("en-US", { year: "numeric" }), 10);
-    this.countDays = Utils.getDaysInMonth(this.month, this.year);
-  }
+//     this.state = {
+//       width: 33,
+//       // month: '',
+//       // year: '',
+//       // countDays: ''
+//     };
+//     this.month = Number.parseInt(this.props.currentDate.toLocaleDateString("en-US", { month: "numeric" }), 10);
+//     this.year = Number.parseInt(this.props.currentDate.toLocaleDateString("en-US", { year: "numeric" }), 10);
+//     this.countDays = Utils.getDaysInMonth(this.month, this.year);
+//   }
 
-  render() {
-    console.log("Table render() :: ", this.props.currentDate.getMonth());
-    return (
-      <React.Fragment>
-        <table>
-        <TableHeader currentDate = {this.props.currentDate} month={this.props.month} year={this.props.year} width = {this.state.width} setModalActive={this.props.setModalActive}></TableHeader>
-           <TableBody currentDate = {this.props.currentDate} month={this.props.month} year={this.props.year} width = {this.state.width}></TableBody>
+//   render() {
+//     console.log("Table render() :: ", this.props.currentDate.getMonth());
+//     return (
+//       <React.Fragment>
+//         <table>
+//         <TableHeader currentDate = {this.props.currentDate} month={this.props.month} year={this.props.year} width = {this.state.width} setModalActive={this.props.setModalActive}></TableHeader>
+//            <TableBody currentDate = {this.props.currentDate} month={this.props.month} year={this.props.year} width = {this.state.width}></TableBody>
 
-        </table>
-        {/* <TableHeader month={this.props.month} year={this.props.year} countDays={countDays} /> */}
-        {/* <TableBody month={this.props.month} year={this.props.year} countDays={countDays} /> */}
-      </React.Fragment>
-    );
-  }
+//         </table>
+//         {/* <TableHeader month={this.props.month} year={this.props.year} countDays={countDays} /> */}
+//         {/* <TableBody month={this.props.month} year={this.props.year} countDays={countDays} /> */}
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const Table = ({allDays, currentDate, teams, setModalActive, width}) => {
+  return(
+      <table>
+        <thead>
+          <TableHeader allDays={allDays} currentDate = {currentDate} teams={teams} setModalActive={setModalActive} width = {width} ></TableHeader>
+        </thead>
+        <tbody>
+          <TableBody allDays={allDays} currentDate = {currentDate} teams={teams} setModalActive={setModalActive} width = {width}></TableBody>
+        </tbody>
+      </table>
+
+  )
 }
 
 export default Table;
